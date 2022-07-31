ff36514bd9a083ea449662f5cfd7401e9eb4b41a
francoisgeyser <fgprojects@pm.me>
2022-08-01 00:23:04 +0300

    http issue

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import { countryActions } from '../store/reducers/countrySlice'
 
 export async function fetchUniversities(dispatch, state) {
   const response = await fetch(
-    'https://universities.hipolabs.com/search?name=university'
+    'http://universities.hipolabs.com/search?name=university'
   )
   const data = await response.json()
   const sanitizedArray = data.reduce((ag, e) => {
